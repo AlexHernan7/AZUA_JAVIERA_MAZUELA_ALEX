4f991e9e7334e4dac627aaa6db5ff77b65bc0161
JavieraAzua <ja.azua@duocuc.cl>
2025-09-30 20:46:10 -0300

    30-09-2025 redireccion

--- proyecto-titulo-2025/apps/vecindApp/frontend/src/app/components/home/home.component.ts
+++ proyecto-titulo-2025/apps/vecindApp/frontend/src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],    
+  imports: [CommonModule, RouterModule],    
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -27,4 +28,23 @@ export class HomeComponent {
     // Por ahora usamos ID fijo 1, en el futuro se podría obtener del usuario
     this.router.navigate(['/juntas', 1]);
   }
+
+  goToCertificado() {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/certificados/residencia/crear']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  goToReserva() {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/reservas']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+
+
 }
